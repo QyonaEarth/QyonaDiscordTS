07ee4362041c79b646b4d053047c6a6bc87628ad
leivaa <adrianleiva212@gmail.com>
2021-12-06 00:00:06 +0100

    feat: :sparkles: Now in suggest Channel only can be used !sugerencia

--- src/bots/Justin/services/events/messageHandler.ts
+++ src/bots/Justin/services/events/messageHandler.ts
@@ -2,15 +2,14 @@ import { Message } from 'discord.js';
 import { Command } from '../../../../domain/interfaces/Command';
 import { Execute } from '../../../../domain/interfaces/Event';
 
-export const name = 'CommandHandler';
+export const name = 'messageHandler';
 export const trigger = 'messageCreate';
 export const description = 'Logs on console all the messages the bot receive';
 
 export const execute: Execute = async (bot, message: Message) => {
   if (
     message.author.bot ||
-    !message.guild ||
-    !message.content.startsWith(bot.config.prefix)
+    !message.guild
   )
     return;
 
@@ -23,6 +22,16 @@ export const execute: Execute = async (bot, message: Message) => {
   const UserCommand: string =
     args[0] != undefined ? args[0].toLowerCase() : 'null';
   args.shift();
+
+  if(message.channelId == bot.config.suggestChannelID && UserCommand!='sugerencia') {
+    const response:Message<boolean> = await message.channel.send(`<@${message.author.id}> En este lugar solo se admiten sugerencias!`)
+    setTimeout(() => response.delete(), 3 * 1000);
+    message.delete();
+    return
+  };
+
+  if(    !message.content.startsWith(bot.config.prefix) )return;
+
   const command: Command | undefined = bot.commands.get(UserCommand)
     ? bot.commands.get(UserCommand)
     : undefined;

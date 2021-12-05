32e4d616a9eeb0cccfecb04f65e08f9ea08ff6ac
leivaa <adrianleiva212@gmail.com>
2021-12-05 01:12:47 +0100

    refactor: :construction: Refactorized index

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,5 @@
-import { Bot } from './domain/bot';
 import 'dotenv/config';
 
-let Qyona: Bot;
-let Will: Bot;
-let Justin: Bot;
-
-if (
-  !process.env.qyona_token ||
-  !process.env.will_token ||
-  !process.env.justin_token
-) {
-  console.log('bot_token not found!');
-} else {
-  Qyona = new Bot(process.env.qyona_token, 'Qyona');
-  Qyona.start();
-  Justin = new Bot(process.env.justin_token, 'Justin');
-  Justin.start();
-  Will = new Bot(process.env.will_token, 'Will');
-  Will.start();
-}
+import './bots/Justin/justin'
+import './bots/Qyona/qyona'
+import './bots/Will/will'
